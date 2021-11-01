bbe2dcaa54ee02c736fc95f2cf2752eb47806dc9
Akwd22 <60796682+Akwd22@users.noreply.github.com>
2021-11-02 00:02:38 +0100

    Fix chemin Doxygen

--- logic/run_doxygen.bat
+++ logic/run_doxygen.bat
@@ -2,11 +2,5 @@
 :loop
 del /s /f /q docs
 doxygen
-echo !!! Membres prives documentes !!!
-echo !!! Membres prives documentes !!!
-echo !!! Membres prives documentes !!!
-echo !!! Membres prives documentes !!!
-echo !!! Membres prives documentes !!!
-echo !!! Membres prives documentes !!!
 pause
 goto loop
