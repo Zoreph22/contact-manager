ecc7f3a010ad80d8ce506f13f2deb7ae8b311c52
Akwd22 <60796682+Akwd22@users.noreply.github.com>
2021-12-09 00:22:06 +0100

    Suppression test

--- cdaa/ui/main.cpp
+++ cdaa/ui/main.cpp
@@ -1,17 +1,10 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QDebug>
-#include <QString>
-
-#include "date.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    Date d(22, 7, 2000);
-    qDebug() << QString::fromStdString(d.toString());
 
     MainWindow w;
     w.show();
