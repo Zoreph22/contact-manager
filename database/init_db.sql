13c43f7faf28cbb49995e963209d0131a19b7413
Akwd22 <60796682+Akwd22@users.noreply.github.com>
2021-12-09 20:39:42 +0100

    Initialisation base de données

---
+++ database/init_db.sql
@@ -0,0 +1,52 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS General;
+CREATE TABLE General (
+  date_suppression    TEXT
+);
+
+DROP TABLE IF EXISTS Contact;
+CREATE TABLE Contact (
+  id                  INTEGER PRIMARY KEY,
+  nom                 TEXT NOT NULL CHECK (nom <> ""),
+  prenom              TEXT NOT NULL CHECK (prenom <> ""),
+  entreprise          TEXT,
+  email               TEXT,
+  tel                 TEXT,
+  photo               TEXT,
+  date_modification   TEXT,
+  date_creation       TEXT NOT NULL DEFAULT (DATE('now'))
+);
+
+DROP TABLE IF EXISTS Interaction;
+CREATE TABLE Interaction (
+  id                  INTEGER PRIMARY KEY,
+  contenu             TEXT NOT NULL CHECK (contenu <> ""),
+  date_creation       TEXT NOT NULL DEFAULT (DATE('now')),
+  id_contact          INTEGER NOT NULL,
+
+  FOREIGN KEY (id_contact) REFERENCES Contact(id)
+    ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS Todo;
+CREATE TABLE Todo (
+  id                  INTEGER PRIMARY KEY,
+  resume              TEXT NOT NULL CHECK (resume <> ""),
+  date_realisation    TEXT NOT NULL DEFAULT (DATE('now')),
+  id_interaction      INTEGER NOT NULL,
+  
+  FOREIGN KEY (id_interaction) REFERENCES Interaction(id)
+    ON DELETE CASCADE
+);
+
+INSERT INTO Contact(id, nom, prenom) VALUES
+  (1, 'Dupont', 'Jean'),
+  (2, 'Durand', 'Lucas');
+
+INSERT INTO Interaction(id, contenu, id_contact) VALUES
+  (1, 'Descriptif interaction 1\n@todo Faire les courses', 1),
+  (2, 'Descriptif interaction 2', 2);
+
+INSERT INTO Todo(id, resume, id_interaction) VALUES
+  (1, 'Faire les courses', 1);
